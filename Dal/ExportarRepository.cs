c997d6c50b1ab50d69ee1ca532323844ed5e449e
Angel Casadiegos <angelcasadiegos@outlook.es>
2020-05-11 11:38:33 -0500

    con

---
+++ Dal/ExportarRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entity;
+
+namespace Dal
+{
+    public class ExportarRepository
+    {
+        private string ruta;
+
+        public void ExportarPorCredito(string nombreEntidad, IList<Entidad> entidades)
+        {
+            AsignarRuta(nombreEntidad);
+            FileStream fileStream = new FileStream(ruta, FileMode.Create);
+            fileStream.Close();
+            Guardado Archivo = new Guardado();
+            foreach (var item in entidades)
+            {
+                Archivo.Guardar(item, ruta);
+            }
+        }
+        public void AsignarRuta(string nombreEntidad)
+        {
+            ruta = @"PagosEntidades.txt";
+            if (nombreEntidad == "Electricaribe")
+            {
+                ruta = @"Electricaribe.txt";
+            }
+            else if (nombreEntidad == "GasCaribe")
+            {
+                ruta = @"GasCaribe.txt";
+            }
+            else
+            {
+                ruta = @"Emdupar.txt";
+            }
+        }
+    }
+}
---
+++ Dal/Guardado.cs
@@ -0,0 +1,22 @@
+﻿using Entity;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dal
+{
+    public class Guardado
+    {        
+        public void Guardar(Entidad entidad, string ruta)
+        {
+            FileStream fileStream = new FileStream(ruta, FileMode.Append);
+            StreamWriter writer = new StreamWriter(fileStream);
+            writer.WriteLine(entidad.ToString());
+            writer.Close();
+            fileStream.Close();
+        }
+    }
+}
